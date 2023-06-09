8f4731f6a12caeed93f656c92f4c12b2e61a6a6e
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2023-06-09 16:10:18 -0400

    Add a model to hold the direction of an object with each of the directional enums

---
+++ Assets/Scripts/Models/Direction.cs
@@ -0,0 +1,13 @@
+﻿using PenguinPeak.Enums.Directions;
+
+namespace PenguinPeak.Models
+{
+    public class Direction
+    {
+        public Vertical Vertical { get; set; } = Vertical.None;
+
+        public Horizontal Horizontal { get; set; } = Horizontal.None;
+
+        public Relative Relative { get; set; } = Relative.None;
+    }
+}
